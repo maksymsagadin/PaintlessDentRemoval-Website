7f12e74b48dda420f36302eec1df75bf3bad1191
maksymsagadin <51523851+maksymsagadin@users.noreply.github.com>
2022-06-21 18:43:51 -0700

    updated work section to show up on mobile

--- src/components/WorkSection/WorkSection.js
+++ src/components/WorkSection/WorkSection.js
@@ -37,10 +37,11 @@ const WorkSection = () => {
 						initial={{ opacity: 0, scale: 0.3 }}
 						transition={{ duration: 0.4 }}
 						animate={animation}
+						ref={ref}
 					>
-						See More Work
+						See More...
 					</WorkHeading>
-					<MasonryGrid ref={ref}>
+					<MasonryGrid>
 						{workData.map((item, index) => (
 							<WorkImageWrapper
 								animate={animation}
