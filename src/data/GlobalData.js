7c433d0293e4d0612435125d4b8c3c4dd5338aa1
maksymsagadin <51523851+maksymsagadin@users.noreply.github.com>
2022-07-09 10:23:23 -0700

    updated primary color

--- src/data/GlobalData.js
+++ src/data/GlobalData.js
@@ -1,6 +1,5 @@
 export const websiteName = 'Dent Duty';
 export const light = 'white';
 export const dark = '#020305';
-export const primary = '#339cc7';
-// export const primary1 = '#23b3fa';
+export const primary = '#11b28a';
 
