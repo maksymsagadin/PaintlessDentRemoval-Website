92eb5d9160b1206c2aaa9497cc33bc075671872d
Maksym Sagadin <maxsagadin@gmail.com>
2022-12-02 12:39:59 -0800

    removed comments

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import GlobalStyle from './globalStyles'
 import HomePage from './pages/HomePage'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-// import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
@@ -11,7 +10,6 @@ const App = () => {
 			<Routes>
 				<Route path="/" element={<HomePage />} />
 			</Routes>
-			{/* <Footer /> */}
 		</Router>
   )
 }
