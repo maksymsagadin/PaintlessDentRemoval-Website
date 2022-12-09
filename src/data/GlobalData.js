e8a1a78b11442cc01ec1ac1ebd7f50be5562c4e3
Maksym Sagadin <maxsagadin@gmail.com>
2022-12-09 12:37:09 -0800

    updated website name and light color

--- src/data/GlobalData.js
+++ src/data/GlobalData.js
@@ -1,5 +1,5 @@
-export const websiteName = 'Dent Duty';
-export const light = 'white';
+export const websiteName = 'Dents & Things';
+export const light = '#cfefe7';
 export const dark = '#020305';
 export const primary = '#11b28a';
 
