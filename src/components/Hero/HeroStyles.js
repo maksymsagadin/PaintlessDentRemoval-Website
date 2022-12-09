447ceea419c401354699161f9d68a74222f3e4fc
Maksym Sagadin <maxsagadin@gmail.com>
2022-12-09 12:40:08 -0800

    added HeroHeading section to add unique styling

--- src/components/Hero/HeroStyles.js
+++ src/components/Hero/HeroStyles.js
@@ -1,19 +1,31 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
+import { primary } from '../../data/GlobalData'
+
 
 export const HeroSection = styled.section`
 	height: 100vh;
-	background: linear-gradient(to bottom, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.5)),
+	background: linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.5)),
 		url('./assets/hero_image0.jpg');
 	background-position: center;
 	background-size: cover;
 	display: flex;
-	align-items: center;
+	padding-top: 7rem;
 	object-fit: contain;
+`
+
+export const HeroHeading = styled.h1`
+    font-size: clamp(3rem, 5vw, 6rem);
+    margin-bottom: 1.5rem;
+    color: ${primary};
+    width: 100%;
+    letter-spacing: 5px;
+    text-align: center;
+	text-shadow: 1px 1px 1px #000;
 `
 
 export const HeroText = styled.p`
 	margin-bottom: 35px;
-	font-size: clamp(0.9rem, 1.5vw, 1.3rem);
+	font-size: clamp(1.1rem, 1.5vw, 1.7rem);
 	line-height: 24px;
 	text-align: center;
 	letter-spacing: 2px;
