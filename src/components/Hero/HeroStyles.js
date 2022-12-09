5f6aa0304d914364b20e7e7ba5947da7f4e12ac8
Maksym Sagadin <maxsagadin@gmail.com>
2022-12-09 13:14:23 -0800

    updated styling

--- src/components/Hero/HeroStyles.js
+++ src/components/Hero/HeroStyles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { primary } from '../../data/GlobalData'
+import { primary, light } from '../../data/GlobalData'
 
 
 export const HeroSection = styled.section`
@@ -9,27 +9,30 @@ export const HeroSection = styled.section`
 	background-position: center;
 	background-size: cover;
 	display: flex;
-	padding-top: 7rem;
+	padding-top: 5rem;
 	object-fit: contain;
 `
 
 export const HeroHeading = styled.h1`
-    font-size: clamp(3rem, 5vw, 6rem);
+    font-size: clamp(3.5rem, 5vw, 6rem);
     margin-bottom: 1.5rem;
     color: ${primary};
     width: 100%;
-    letter-spacing: 5px;
+    letter-spacing: 7px;
     text-align: center;
 	text-shadow: 1px 1px 1px #000;
 `
 
 export const HeroText = styled.p`
 	margin-bottom: 35px;
-	font-size: clamp(1.1rem, 1.5vw, 1.7rem);
+	font-size: clamp(1.3rem, 1.5vw, 1.7rem);
 	line-height: 24px;
 	text-align: center;
 	letter-spacing: 2px;
-	color: white;
+	color: ${light};
+	text-shadow: 0.5px 0.5px 0.5px #000;
+
+
 `
 
 export const ButtonWrapper = styled.div`
