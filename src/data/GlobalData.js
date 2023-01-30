3d6efbb9bdbfe8d817bb558e13750744cd2de322
Maksym Sagadin <maxsagadin@gmail.com>
2023-01-30 11:29:51 -0800

    updated name

--- src/data/GlobalData.js
+++ src/data/GlobalData.js
@@ -1,4 +1,4 @@
-export const websiteName = 'Dents & Things';
+export const websiteName = 'Dents N Dings';
 export const light = '#cfefe7';
 export const dark = '#020305';
 export const primary = '#11b28a';
