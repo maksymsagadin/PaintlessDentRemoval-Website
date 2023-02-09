6895442b7c5ee5e12ab5dcb978ef035ea36b4d6d
Maksym Sagadin <maxsagadin@gmail.com>
2023-02-08 16:58:20 -0800

    Implemented Apollo Client

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import GlobalStyle from './globalStyles'
+import { useSiteData } from './services/useSiteData'
 import HomePage from './pages/HomePage'
+import { VscTerminalPowershell } from 'react-icons/vsc'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 const App = () => {
+	const { error, data, loading } = useSiteData()
+	
+	if (loading) return <div><VscTerminalPowershell /></div>
+	if( error) return <div>Error : {error.message}</div>
+
 	return (
     	<Router>
 			<GlobalStyle />
 			<Routes>
-				<Route path="/" element={<HomePage />} />
+				<Route path="/" element={<HomePage heroSection={data.heroSection} aboutSection={data.aboutSection} servicesSection={data.servicesSection} gallerySection={data.gallerySection} />} />
 			</Routes>
 		</Router>
-  )
+  	)
 }
 
 export default App
